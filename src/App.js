3b60f4a0b50b87f3eac7d97d80778b1f08ab94ef
chetan <chetansuthar734@gmail.com>
2023-06-13 14:04:14 +0530

    commite

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React,{useState} from 'react';
+
+import './App.css';
+
+import Header from  "./Mycomponent/Header";
+  
+
+function App()
+ 
+  {
+
+ return( 
+
+ <div className='a'>
+<Header/>  
+ </div>
+
+
+
+ );
+
+}
+
+export default App;
